25e070515fc9ebaf2b5fa158a99869683f2a6b3f
MyBoyDoru <denis.leonte123@gmail.com>
2022-04-14 00:54:59 +0300

    Implemented the CLIApp in the main function

--- Project OOP/main.cpp
+++ Project OOP/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include "CLIApp.h"
+#include "Controller.h"
+#include "Shop.h"
 using std::cout;
 
 int main()
 {
-
+	CLIApp app(Controller(Shop("DenisTechs")));
+	app.Start();
 	return 0;
 }
